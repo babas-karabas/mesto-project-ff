1079a1c5ea2e2567c438d008a1ccda48744f8790
Babaskina Elena <babas.karabas@yandex.ru>
2025-02-10 20:50:06 +0300

    настройка вебпака

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import './pages/index.css';
+
+const cardTemplate = document.querySelector('#card-template').content; 
+const placesList = document.querySelector('.places__list');
+const card = cardTemplate.querySelector('.card');
+
+function makeCard(cardItem, removeElement) {
+
+  const newCard = card.cloneNode(true);
+  const deleteButton = newCard.querySelector('.card__delete-button');
+  const cardImage = newCard.querySelector('.card__image');
+
+  cardImage.src = cardItem.link; 
+  cardImage.alt = cardItem.name;
+  newCard.querySelector('.card__title').textContent = cardItem.name;
+  
+  deleteButton.addEventListener('click', function () {
+    removeElement(newCard);
+  }); 
+
+  return newCard;
+};
+
+function removeElement(elem) {
+  elem.remove();
+};
+
+
+const cards = initialCards.map(elem => {
+  return makeCard(elem, removeElement);
+});
+
+cards.forEach(elem => {
+  placesList.append(elem);
+});
+
+
+
+
+
+
+
